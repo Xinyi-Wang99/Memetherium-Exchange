5225a30c2b3f7c4f493850994db8a0a6a965e925
Xinyi-Wang99 <Xinyi.Wang@colostate.edu>
2021-04-07 23:24:53 -0600

    upload images to IPFS

---
+++ memethereum_dapp/memethereum_front_end/src/pages/Ipfs.js
@@ -0,0 +1,60 @@
+import React, {Component} from "react";
+
+const IPFS = require('ipfs-api')
+const ipfs = new IPFS({host: 'ipfs.infura.io', port:5001, protocol:'https'})
+
+export default class Ipfs extends Component{
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            buffer: [],
+            ipfsHash:null
+        }
+
+        this.captureFile= this.captureFile.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    captureFile(event){
+        event.preventDefault()
+        const file = event.target.files[0]
+        const reader = new window.FileReader()
+        reader.readAsArrayBuffer(file)  //converting the file into an array
+        reader.onloadend = () =>{
+            this.setState({buffer: Buffer(reader.result )})
+        }
+    }
+
+    onSubmit(event){
+        event.preventDefault()
+        ipfs.files.add(this.state.buffer, (error, result) => {
+            if(error){
+                console.error(error)
+                return
+            }
+            this.setState({ipfsHash : result[0].hash})
+        })
+    }
+
+
+    render() {
+        return(
+            <div>
+                <a href="#">Meme Upload</a>
+                <main className="container">
+                    <h1>Your Meme</h1>
+                    <p> This image is stored on IPFS & The Ethereum Blockchain!</p>
+                    <img src ={`https://ipfs.io/ipfs/${this.state.ipfsHash}`} alt =""/>
+                    <h2>Upload Meme</h2>
+                    <form onSubmit = {this.onSubmit}>
+                        <input type="file" onChange = {this.captureFile}/>
+                        <input type="submit" />
+                    </form>
+                </main>
+            </div>
+        );
+    }
+
+}
+
