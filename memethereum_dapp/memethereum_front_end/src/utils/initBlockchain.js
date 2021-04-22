6698d6d50e0d35c12c0f6e573c283ccad96b70b7
Xinyi-Wang99 <Xinyi.Wang@colostate.edu>
2021-04-22 14:07:05 -0600

    write the meme to blockchain

--- memethereum_dapp/memethereum_front_end/src/utils/initBlockchain.js
+++ memethereum_dapp/memethereum_front_end/src/utils/initBlockchain.js
@@ -7,12 +7,13 @@ const initBlockchain = async () => {
     window.ethereum.enable().then(provider = new ethers.providers.Web3Provider(window.ethereum));
 
     const signer = await provider.getSigner()
+    console.log("READ ABI", MemetheriumContract.abi);
+
     console.log("signer", signer);
     const userAddress =  await signer.getAddress();
     console.log("user address", userAddress);
 
     let MEME = null;
-    console.log("READ ABI", MemetheriumContract.abi);
     console.log(MemetheriumContract);
     const MEMEabi = MemetheriumContract.abi;
     MEME = new ethers.Contract('0x74D7EE64E10E877ce1aAdfbA458b3F7ec583Fa1E', MEMEabi, signer);
