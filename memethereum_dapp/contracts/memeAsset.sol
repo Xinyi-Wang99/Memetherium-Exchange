e10de7a8bbe086fd00512716034b50d15102d5f9
Jacob Barth <jakebart@cs.colostate.edu>
2021-04-26 15:02:23 -0600

    added openzeppelin and memeAsset.sol

---
+++ memethereum_dapp/contracts/memeAsset.sol
@@ -0,0 +1,8 @@
+pragma solidity >=0.5.0 <0.6.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract memeAsset is ERC721 {
+
+}
